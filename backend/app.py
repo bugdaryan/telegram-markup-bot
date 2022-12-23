f6c23b0fcf4122fd739490aaab1eb0405b4b41a7
bugdaryan <bugdaryan@gmail.com>
2022-12-24 02:47:15 +0400

    Update app imports

--- backend/app.py
+++ backend/app.py
@@ -4,20 +4,13 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restx import Api
 from config import Config
+from flask_httpauth import HTTPBasicAuth
 
-
-from flask_login import (
-    UserMixin,
-    login_user,
-    LoginManager,
-    current_user,
-    logout_user,
-    login_required,
-)
 
 db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
+auth = HTTPBasicAuth()
 
 def create_app():
     app = Flask(__name__)
@@ -28,13 +21,6 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = Config.DATABASE_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-    # login_manager = LoginManager()
-    # login_manager.session_protection = "strong"
-    # login_manager.login_view = "login"
-    # login_manager.login_message_category = "info"
-
-
-    # login_manager.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
