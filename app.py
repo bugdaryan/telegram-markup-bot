72f24262cb9cdc130278d795a7b2d00358105ed6
bugdaryan <bugdaryan@gmail.com>
2022-12-25 14:28:08 +0400

    Update clean app and main

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
-from flask_restx import Api
 from config import Config
 from flask_httpauth import HTTPBasicAuth
-
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
-bcrypt = Bcrypt()
 auth = HTTPBasicAuth()
 
+
 def create_app():
-    app = Flask(__name__)
-    api = Api(app, version='1.0', title='Markup bot', description='Markup bot API documentation', doc='/docs')
-
-
+    app = Flask(__name__, template_folder='templates')
+    login_manager = LoginManager(app)
     app.secret_key = Config.SECRET_KEY
     app.config['SQLALCHEMY_DATABASE_URI'] = Config.DATABASE_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     db.init_app(app)
     migrate.init_app(app, db)
-    bcrypt.init_app(app)
     
-    return app, api
+    return app, login_manager
 
-app, api = create_app()
+app, login_manager = create_app()
--- main.py
+++ main.py
@@ -4,14 +4,27 @@ from models import User, Image, Annotation, Label
 from api import register, get_token, get_labels
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin
-from admin import ImageModelView
+from admin import ImageModelView, MyAdminIndexView, LoginForm, MyModelView
+from flask import redirect, url_for
+import flask_login as login
 
+def init_login():
+    login_manager = login.LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.query(User).get(user_id)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return redirect(url_for('admin.index'))
 
 if __name__ == '__main__':
     app.app_context().push()
     db.create_all()
-
-
+    
     with open(Config.INIT_SQL_FILE, 'r') as f:
         sql_script = f.readlines()
     sql_script = '\n'.join(sql_script)
@@ -22,12 +35,13 @@ if __name__ == '__main__':
             if len(script):
                 db.session.execute(script)
                 db.session.commit()
+    init_login()
 
-    admin = Admin(app, name='saitama', template_mode='bootstrap3')
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Annotation, db.session))
-    admin.add_view(ModelView(Label, db.session))
-    admin.add_view(ImageModelView(Image, db.session, name='Image'))
-    
+    admin = Admin(app, name='admin', template_mode='bootstrap4', index_view=MyAdminIndexView(),  base_template='my_master.html')
+    admin.add_view(MyModelView(User, db.session))
+    admin.add_view(MyModelView(Annotation, db.session))
+    admin.add_view(MyModelView(Label, db.session))
+    admin.add_view(ImageModelView(Image, db.session))
+
     app.debug = Config.DEBUG
     app.run(port=Config.PORT)
