48d663a8e30b94dcade4f37a5e061a173aaf3a70
bugdaryan <bugdaryan@gmail.com>
2022-12-25 14:25:02 +0400

    Update admin name and password

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,4 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 INSERT INTO users (id, username, password_hash, points, is_admin) VALUES
-(uuid_generate_v4(), 'SAITAMA', 'pbkdf2:sha256:260000$joJByQ0nx4he67K0$ee8be18f5df94bbbadbcf5817cce73e006b4afb1d8691cc0899b475d918b523b', 0, true) ON CONFLICT DO NOTHING;
+(uuid_generate_v4(), 'admin', 'pbkdf2:sha256:260000$7SzoT7HQXf9Xf6TE$dd9c61baa0865ab5c57eec4981b041a5e2bd31e66c50d84f320fc2f4e03af415', 0, true) ON CONFLICT DO NOTHING;
