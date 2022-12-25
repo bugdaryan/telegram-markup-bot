3df48d33e12852dd9ba50b5dcf1dae4816d9bd6d
bugdaryan <bugdaryan@gmail.com>
2022-12-26 03:54:38 +0400

    Update labels api to send labels and ids separately

--- api/LabelsAPI.py
+++ api/LabelsAPI.py
@@ -5,6 +5,6 @@ from config import Config
 
 @app.route('/api/labels', methods=['GET'])
 def get_labels():
-    labels = Label.query.with_entities(Label.label_name).distinct().all()
-    labels = [label[0] for label in labels]
-    return (jsonify({'labels': labels}), 200)
+    labels = Label.query.with_entities(Label.name, Label.id).distinct().all()
+    labels_dict = {'label': [label[0] for label in labels], 'id':[label[1] for label in labels]}
+    return (jsonify(labels_dict), 200)
