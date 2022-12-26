604dd6efb8b439a1cf24e4a3ad87dc5bf930b432
bugdaryan <bugdaryan@gmail.com>
2022-12-26 05:53:04 +0400

    Update config ,testing yaml

--- config.py
+++ config.py
@@ -15,7 +15,6 @@ class Config(ABC):
         DATABASE_URL = os.getenv('LOCAL_DB')
 
     DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://')
-    SECRET_KEY = os.getenv('BCRYPT_SECRET_KEY')
     TELEGRAM_SECRET_KEY = os.getenv('TELEGRAM_SECRET_KEY')
     PASSWORD_LENGTH = 10
     INIT_SQL_FILE = 'db/init.sql'
